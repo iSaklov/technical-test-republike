254b44243354ee881672cf23b55d6657c3eba8e0
iSaklov <ivansaklov@gmail.com>
2024-03-12 11:56:16 +0100

    feat: Initialize project with client and server setup
    - Initialized a React application with TypeScript and integrated Tailwind CSS for styling.
    - Set up a Node.js server using Express framework.
    - Added ESLint and Prettier for code linting and formatting across the client and server codebases.
    - Configured scripts for concurrent running of the client and server for development ease.
    - Tailored .gitignore files to exclude node_modules, build artifacts, and environment files from version control.
    
    This setup provides a solid foundation for the development of a full-stack application, ensuring code quality and consistency.

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="font-bold text-5xl">Hello Republike !</h1>
+    </div>
+  )
+}
+
+export default App
